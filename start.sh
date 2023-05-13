e3635030088a47757d1870a710cc33aeeb51e580
Damion <the@dbrown.id.au>
2023-05-13 17:55:17 +1000

    Turns out batchmode isn't recognized anymore

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 steamcmd +runscript /run.txt
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/rust/RustDedicated_Data/Plugins
 usermod -d /rust nobody
-runuser -u nobody -- /rust/RustDedicated -batchmode -logfile 2>&1
+runuser -u nobody -- /rust/RustDedicated 2>&1
